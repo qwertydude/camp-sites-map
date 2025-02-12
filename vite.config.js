c2cb75a6bc06626fc6995858629e724b8458d20d
Chris Howard <reg@341design.com.au>
2025-02-12 23:33:41 +1100

    Update Vite config to use relative path for static assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
+
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	envPrefix: 'PUBLIC_'
+	envPrefix: 'PUBLIC_',
+	server: {
+    fs: {
+      allow: [
+        path.resolve(__dirname, 'static'),
+      ],
+    }
+	}
+
 });
