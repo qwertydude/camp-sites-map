bd6865bcbf267707dd0a8893fbeac1e28d3b8473
Chris Howard <reg@341design.com.au>
2025-02-12 20:58:08 +1100

    Replace route type emojis with icons

--- src/lib/components/CampSiteMap.svelte
+++ src/lib/components/CampSiteMap.svelte
@@ -59,9 +59,9 @@
 						<p>Distance: ${distanceKm} km</p>
 						<p>Duration: ~${duration} minutes</p>
 						<div class="travel-modes">
-							<button class="travel-mode-btn" on:click="{() => setTravelMode('foot')}" style="font-size: 1.5em;">🚶</button>
-							<button class="travel-mode-btn" on:click="{() => setTravelMode('bike')}" style="font-size: 1.5em;">🚴</button>
-							<button class="travel-mode-btn" on:click="{() => setTravelMode('car')}" style="font-size: 1.5em;">🚗</button>
+							<button class="travel-mode-btn" on:click="{() => setTravelMode('foot')}" style="font-size: 1.5em;"><img src="/path/to/walk-icon.png" alt="Walk" /></button>
+							<button class="travel-mode-btn" on:click="{() => setTravelMode('bike')}" style="font-size: 1.5em;"><img src="/path/to/bike-icon.png" alt="Bike" /></button>
+							<button class="travel-mode-btn" on:click="{() => setTravelMode('car')}" style="font-size: 1.5em;"><img src="/path/to/car-icon.png" alt="Car" /></button>
 						</div>
 					</div>`
 				)
