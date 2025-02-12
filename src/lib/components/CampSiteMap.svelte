822a966e4b6142f0170af6c0633e39f7c5d93055
Chris Howard <reg@341design.com.au>
2025-02-12 23:22:24 +1100

    Update icon paths to use relative paths for both development and production

--- src/lib/components/CampSiteMap.svelte
+++ src/lib/components/CampSiteMap.svelte
@@ -59,9 +59,9 @@
 						<p>Distance: ${distanceKm} km</p>
 						<p>Duration: ~${duration} minutes</p>
 						<div class="travel-modes">
-							<button class="travel-mode-btn" on:click="{() => setTravelMode('foot')}" style="font-size: 1.5em;"><img src="/lib/assets/icons/walk-icon.svg" alt="Walk" /></button>
-							<button class="travel-mode-btn" on:click="{() => setTravelMode('bike')}" style="font-size: 1.5em;"><img src="/lib/assets/icons/ride-icon.svg" alt="Ride" /></button>
-							<button class="travel-mode-btn" on:click="{() => setTravelMode('car')}" style="font-size: 1.5em;"><img src="/lib/assets/icons/drive-icon.svg" alt="Drive" /></button>
+							<button class="travel-mode-btn" on:click="{() => setTravelMode('foot')}" style="font-size: 1.5em;"><img src="/assets/icons/walk-icon.svg" alt="Walk" /></button>
+							<button class="travel-mode-btn" on:click="{() => setTravelMode('bike')}" style="font-size: 1.5em;"><img src="/assets/icons/ride-icon.svg" alt="Ride" /></button>
+							<button class="travel-mode-btn" on:click="{() => setTravelMode('car')}" style="font-size: 1.5em;"><img src="/assets/icons/drive-icon.svg" alt="Drive" /></button>
 						</div>
 					</div>`
 				)
