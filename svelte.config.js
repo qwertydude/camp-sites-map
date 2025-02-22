cea63d02230a4b277da87bd3a6dbb1077e0aa859
Chris Howard <reg@341design.com.au>
2025-02-23 04:01:31 +1100

    trying adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,21 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+//import adapter from '@sveltejs/adapter-cloudflare';
 //import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 
+import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
 		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		})
 	},
 	preprocess: vitePreprocess()
