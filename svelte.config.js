ff1d045d58d8a5eca96fae33610b24402baf7532
Chris Howard <reg@341design.com.au>
2025-02-23 16:11:58 +1100

    feat: configure project for Cloudflare deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-//import adapter from '@sveltejs/adapter-cloudflare';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 
