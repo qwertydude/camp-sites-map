0bb83ce8beb36489aa738541f50585743a09c0a9
Chris Howard <reg@341design.com.au>
2025-02-23 16:47:14 +1100

    attempt to get cloudflare pages working

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,23 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 /** @type {import('@sveltejs/kit').Config} */
 
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
 	},
 	preprocess: vitePreprocess()
 };
